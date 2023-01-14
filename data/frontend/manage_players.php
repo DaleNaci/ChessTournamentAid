b93b6649ce1dc67cb119e19d4405450e1275d41e
DaleNaci <dtn22@njit.edu>
2023-01-13 19:27:49 -0500

    Added basic structure of manage players php file

---
+++ data/frontend/manage_players.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+$tournamentName = $_SESSION["tournament-name"];
+$tournamentStatus = $_SESSION["tournament-status"];
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+
+    <body>
+
+        <div class="box">
+            <h1 class="box__h1">Manage Players</h1>
+        </div>
+    </body>
+</html>
